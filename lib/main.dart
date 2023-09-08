e4b8dd3e72b9d34f8e34c49fe985cbbbe5e77853
leekina <leemh332@gmail.com>
2023-09-08 16:22:09 +0900

    SETTING : FCM TEST를 위한 프로젝트입니다. 2023/ 09/ 08 이상없음

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:fcm_test/fcmPage.dart';
-import 'package:fcm_test/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:permission_handler/permission_handler.dart';
 
 @pragma('vm:entry-point')
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
